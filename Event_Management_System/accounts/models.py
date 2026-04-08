6a6d0b62fb76f79ca45a52c15f473db36bab25be
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-08 08:47:49 +0530

    Update in login and singup logic

--- Event_Management_System/accounts/models.py
+++ Event_Management_System/accounts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-
 class UserManager(BaseUserManager):
 
     def create_user(self, email, username, password=None, **extra_fields):
@@ -21,50 +20,42 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-
-    def create_superuser(self, email, username, phone_number, password=None, **extra_fields):
+    def create_superuser(self, email, username, password=None, **extra_fields):
 
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True")
-
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True")
+        if not extra_fields.get("phone_number"):
+            raise ValueError("Phone number is required")
 
         return self.create_user(
             email=email,
             username=username,
             password=password,
-            phone_number=phone_number,
             **extra_fields
         )
 
 
-
-   
 class User(AbstractUser):
-    email=models.EmailField(unique=True)
-    phone_number=models.CharField(max_length=15,unique=True)
 
-    profile_image=models.ImageField(upload_to="/profile_images",blank=True,null=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
 
-    is_email_verified=models.BooleanField(default=False)
-    is_phone_verified=models.BooleanField(default=False)
+    profile_image = models.ImageField(upload_to="profile_images/", blank=True, null=True)
 
-    is_deleted=models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=False)
+    is_phone_verified = models.BooleanField(default=False)
 
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-
     REQUIRED_FIELDS = ["username", "phone_number"]
 
     def __str__(self):
         return self.username
-    
