5601c4379df8ff6fa0c47b61c56efec5fa989e64
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-08 09:04:33 +0530

    Update in login and singup logic

--- Event_Management_System/Event_Management_System/settings.py
+++ Event_Management_System/Event_Management_System/settings.py
@@ -1,5 +1,5 @@
 
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,7 +100,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
