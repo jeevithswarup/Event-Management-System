e1cad4803eabf45d9fe7fd043ac55dacb37c28cc
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-04-09 10:04:36 +0530

    Update in login and singup logic

--- Event_Management_System/accounts/admin.py
+++ Event_Management_System/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("id", "username", "email", "phone_number", "is_staff", "is_active")
+    search_fields = ("email", "username", "phone_number")
+    ordering = ("id",)
+
+
+admin.site.register(User, CustomUserAdmin)
+
--- Event_Management_System/accounts/urls.py
+++ Event_Management_System/accounts/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from accounts.views import home
 
 urlpatterns = [
- 
+ path('',home)
 ] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
